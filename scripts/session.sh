9c98857fa099d76624c3cdf4d0ed18851cb0c62c
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-12-03 12:12:35 -0500

    Delete unused scripts and update session script

--- scripts/session.sh
+++ scripts/session.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
+# If an argument is provided, use it as the selected directory
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(find ~/projects/hive ~/projects/personal ~/projects ~/scripts ~/.config -mindepth 1 -maxdepth 1 -type d | fzf)
+  # Otherwise, use fzf to interactively select a directory from the specified paths
+  selected=$(find ~/projects/hive ~/projects/personal ~/projects ~/scripts -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
+# If no directory is selected (user pressed Esc or fzf returned nothing), exit
 if [[ -z $selected ]]; then
   exit 0
 fi 
 
+# Extract the directory base name, replace dots with underscores, and use as session name
 selected_name=$(basename $"$selected" | tr . _)
-tmux_running=$(pgrep tmux)
 
+# If the script is not running inside an exisitng tmux session ($TMUX is unset)
 if [[ -z $TMUX ]]; then
+  # Check if session with the name already exists
   if tmux has-session -t=$selected_name 2> /dev/null; then
+      # If the session already exists, attach to it
       tmux attach-session -t $selected_name
   else
-    tmux new-session -s $selected_name -c $selected
+    # If the session doesn't exist, create a new tmux session
+    tmux new-session -s $selected_name -c $selected -n code \; \
+      new-window -n shell -c $selected \; \
+      select-window -t code
   fi
     exit 0
 fi
 
+# If tmux is already running and the session does not exist, create in detached mode
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+    tmux new-session -ds $selected_name -c $selected -n code
+    tmux new-window -t $selected_name:1 -n shell
 fi
 
 tmux switch-client -t $selected_name
+tmux select-window -t $selected_name:0
