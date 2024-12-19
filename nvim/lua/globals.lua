84ebe85a7ccb27afbf5fe7eddb0c78d828e81e14
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-12-19 12:01:31 -0500

    Add globals file

---
+++ nvim/lua/globals.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.obsidian_vaults = {
+  macos = "~/Documents/macos_vault/",
+  linux = "~/Documents/linux_vault/",
+}
+
+M.get_vault_path = function(vault_name)
+  local path = M.obsidian_vaults[vault_name]
+  if not path then
+    return nil
+  end
+  return vim.fn.expand(path)
+end
+
+return M
