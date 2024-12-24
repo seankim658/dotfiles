3a8baae1c1e3aef3944996ff97334e1e52401280
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-12-23 22:06:10 -0500

    Allow for obsidian script usage within tmux

--- scripts/obsidian.sh
+++ scripts/obsidian.sh
@@ -7,8 +7,20 @@ else
   VAULT_PATH="$HOME/Documents/legion_vault/"
 fi
 
-if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-  tmux attach-session -t "$SESSION_NAME"
-else
-  tmux new-session -s "$SESSION_NAME" -c "$VAULT_PATH"
+# If not in tmux, create or attach to session
+if [[ -z $TMUX ]]; then
+  if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    tmux attach-session -t "$SESSION_NAME"
+  else
+    tmux new-session -s "$SESSION_NAME" -c "$VAULT_PATH"
+  fi
+  exit 0
 fi
+
+# If in tmux and session doesn't exist, create it in detached mode
+if ! tmux has-session -t="$SESSION_NAME" 2>/dev/null; then
+  tmux new-session -ds "$SESSION_NAME" -c "$VAULT_PATH"
+fi
+
+# Switch to the obsidian session
+tmux switch-client -t "$SESSION_NAME"
