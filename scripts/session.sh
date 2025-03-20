c2dedaaa233235a9c2f2bf6429e4f2385a453ed9
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-03-19 23:55:29 -0400

    Add notes window

--- scripts/session.sh
+++ scripts/session.sh
@@ -2,6 +2,12 @@
 
 SEARCH_PATHS=(~/projects/hive ~/projects/personal ~/projects/misc ~/projects ~/scripts)
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  NOTES_PATH="$HOME/Documents/macos_vault/"
+else
+  NOTES_PATH="$HOME/Documents/legion_vault/"
+fi
+
 # If an argument is provided, use it as the selected directory
 if [[ $# -eq 1 ]]; then
   selected=$1
@@ -28,6 +34,7 @@ if [[ -z $TMUX ]]; then
     # If the session doesn't exist, create a new tmux session
     tmux new-session -s $selected_name -c $selected -n code \; \
       new-window -n shell -c $selected \; \
+      new-window -n notes -c $NOTES_PATH \; \
       select-window -t code
   fi
     exit 0
@@ -36,7 +43,8 @@ fi
 # If tmux is already running and the session does not exist, create in detached mode
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected -n code
-    tmux new-window -t $selected_name:1 -n shell
+    tmux new-window -t $selected_name:1 -n shell -c $selected
+    tmux new-window -t $selected_name:2 -n notes -c $NOTES_PATH
 fi
 
 tmux switch-client -t $selected_name
