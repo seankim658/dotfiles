70a11a7aaf3988f5f35d3c835640d285e12b4994
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-05-09 00:00:32 -0400

    Frontmatter tweak

--- nvim/lua/utils/markdown.lua
+++ nvim/lua/utils/markdown.lua
@@ -1,36 +1,37 @@
 local M = {}
 
+local function title_case(str)
+  str = string.gsub(str, "[-_]", " ")
+  str = string.gsub(str, "(%a)([%w]*)", function(first, rest)
+    return string.upper(first) .. string.lower(rest)
+  end)
+
+  return str
+end
+
 M.insert_frontmatter = function()
-  local frontmatter = [[---
+  local filename = vim.fn.expand "%:t:r"
+  local title = title_case(filename)
 
-tags:  
-created:  
+  local datetime = os.date "%Y-%m-%d %H:%M:%S"
 
----
-]]
+  local frontmatter = {
+    "---",
+    "> title: " .. title,
+    ">",
+    "> tags: ",
+    ">",
+    "> created: " .. datetime,
+    "---",
+  }
 
-  -- Get the total number of linse in the buffer
-  local line_count = vim.api.nvim_buf_line_count(0)
+  -- Insert at the top of the file
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, frontmatter)
 
-  -- Check if the last line is empty
-  local last_line = vim.api.nvim_buf_get_lines(0, line_count - 1, line_count, false)[1]
-  local needs_blank_line = last_line ~= ""
+  -- Move cursor to the title line
+  vim.api.nvim_win_set_cursor(0, { 4, 4 })
 
-  -- Insert blank line if needed, then the frontmatter
-  if needs_blank_line then
-    vim.api.nvim_buf_set_lines(0, line_count, line_count, false, { "", "" })
-    vim.api.nvim_buf_set_lines(0, line_count + 2, line_count + 2, false, vim.split(frontmatter, "\n"))
-    -- Move cursor after the frontmatter
-    vim.api.nvim_win_set_cursor(0, { line_count + 2 + 5, 0 })
-  else
-    -- If last line is already blank, just insert another blank line and then the frontmatter
-    vim.api.nvim_buf_set_lines(0, line_count, line_count, false, { "" })
-    vim.api.nvim_buf_set_lines(0, line_count + 1, line_count + 1, false, vim.split(frontmatter, "\n"))
-    -- Move cursor after the frontmatter
-    vim.api.nvim_win_set_cursor(0, { line_count + 1 + 5, 0 })
-  end
-
-  vim.notify("Added frontmatter to the bottom of the file", vim.log.levels.INFO)
+  vim.notify("Added frontmatter to the top of the file", vim.log.levels.INFO)
 end
 
 return M
