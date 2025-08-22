02a18ad08d75c10d164697fd13555ea67c45afa1
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-08-22 16:12:52 -0500

    Add obsidian nvim functionality

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -95,6 +95,97 @@ return {
       }
     end,
   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    event = function()
+      local vault_path = globals.get_vault_path "main"
+      if vault_path then
+        return {
+          "BufReadPre " .. vault_path .. "*.md",
+          "BufNewFile " .. vault_path .. "*.md",
+        }
+      end
+      return {}
+    end,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "main",
+          path = globals.get_vault_path "main",
+        },
+      },
+
+      new_notes_location = "current_dir",
+
+      disable_frontmatter = true,
+
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        substitutions = {
+          yesterday = function()
+            return os.date("%Y-%m-%d", os.time() - 86400)
+          end,
+          tomorrow = function()
+            return os.date("%Y-%m-%d", os.time() + 86400)
+          end,
+        },
+      },
+
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+
+      ui = {
+        enable = true,
+        update_debounce = 200,
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "✓", hl_group = "ObsidianDone" },
+          [">"] = { char = "→", hl_group = "ObsidianRightArrow" },
+          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+      },
+
+      attachments = {
+        img_folder = "assets/imgs",
+      },
+
+      yaml_parser = "native",
+
+      note_id_func = function(title)
+        return title
+      end,
+    },
+  },
   require "configs.cmp",
   require "configs.nvim-tree",
   require "configs.nvim-treesitter",
--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -4,5 +4,6 @@ M.time = require "utils.timestamp"
 M.buffer = require "utils.buffer"
 M.diagnostic = require "utils.diagnostics"
 M.typo = require "utils.typo"
+M.obsidian = require "utils.obsidian"
 
 return M
